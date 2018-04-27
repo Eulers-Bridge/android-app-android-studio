575c2cd722b199789ea5b3c456e79cd4cc939137
Sebastian Jachec <sjachec@me.com>
2018-04-27 13:48:55 +0100

    Bump version number to 1.0.22, VC #23

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -8,8 +8,8 @@ object Versions {
     const val min_sdk = 16
     const val target_sdk = 27
     const val compile_sdk = 27
-    const val version_code = 15
-    const val version_name = "1.0.14"
+    const val version_code = 23
+    const val version_name = "1.0.22"
 
     const val ktx = "0.3"
     const val multidex = "1.0.3"
