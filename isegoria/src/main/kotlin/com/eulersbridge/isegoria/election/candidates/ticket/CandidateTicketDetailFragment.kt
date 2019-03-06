2b110660f7176d9384b3aba17fba5e9532223fb4
Errol Hassall <Errol.Hassall99@gmail.com>
2019-03-06 16:04:06 +1100

    Fixed up the election ticket detail rows
    Each row was incorrectly rendering. We added the party code, the placeholder for the profile picture which will be added once the api gives it to us. Changed the icon of the linking to the profile for a candidate which will be added when we complete the candidate page.

--- isegoria/src/main/kotlin/com/eulersbridge/isegoria/election/candidates/ticket/CandidateTicketDetailFragment.kt
+++ isegoria/src/main/kotlin/com/eulersbridge/isegoria/election/candidates/ticket/CandidateTicketDetailFragment.kt
@@ -6,6 +6,7 @@ import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.design.widget.TabLayout
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.util.TypedValue
 import android.view.Gravity
 import android.view.LayoutInflater
